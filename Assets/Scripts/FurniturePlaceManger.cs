d6b0177d6b6495e5c172333e592800afc7708a3d
Marwan Osama EL-Safy <MarwanOsama1997@gmail.com>
2019-09-17 17:16:34 +0200

    furniture v1 first day

---
+++ Assets/Scripts/FurniturePlaceManger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class FurniturePlaceManger : MonoBehaviour
+{
+    public VRTK_StraightPointerRenderer pointerRenderer;
+    public GameObject furnitureModel;
+
+    public void GenerateFurnitureModel()
+    {
+        print("position to place :" + pointerRenderer.GetActualCursor().transform.position);
+        GameObject @object =  Instantiate(furnitureModel, pointerRenderer.GetActualCursor().transform.position, pointerRenderer.GetActualCursor().transform.rotation);
+        @object.transform.parent = pointerRenderer.transform;
+    }
+
+}
